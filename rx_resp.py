ff3fe888603329050b419a2c9096ca114af052c4
Darek <dkrysmann@gmail.com>
2020-11-07 00:19:45 +0100

    done for tonight

--- rx_resp.py
+++ rx_resp.py
@@ -3,9 +3,11 @@ import json
 with open('api_response.json', 'r') as fp:
     data = json.load(fp)
     
-for i in data['Blocks']:
-    try:
-        print({i['Text']})
-    except:
-        pass
+# for i in data['Blocks']:
+#     try:
+#         print({i['Text']})
+#     except:
+#         pass
     
+ d_items = data.items()
+ print(d_items)
